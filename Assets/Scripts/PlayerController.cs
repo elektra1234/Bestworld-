656ca69fb466e9ce99470c94e41100c95b492a83
elektra1234 <procode41@gmail.com>
2025-06-12 16:19:25 -0600

    Added basic player movement script 🚀

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(moveX, 0, moveZ);
+    }
+}
